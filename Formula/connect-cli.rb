455c399a48ef346436b7b0e5078aed7a2a6bf327
Francesco Faraone <francesco.faraone@cloudblue.com>
2021-11-29 12:34:16 +0100

    Bump to 24.0

--- Formula/connect-cli.rb
+++ Formula/connect-cli.rb
@@ -3,8 +3,8 @@ class ConnectCli < Formula
 
   desc "CloudBlue Connect Command Line Interface"
   homepage "https://connect.cloudblue.com"
-  url "https://files.pythonhosted.org/packages/9f/5d/635ee0508b8be391f0e1f7ffa2eda4fe256527d71ad0849af707ea036535/connect-cli-22.19.tar.gz"
-  sha256 "fd3975cda2632ee58e0ca7a422ffd21cb1777fdbf4e58e1d6f573473b8f5f335"
+  url "https://files.pythonhosted.org/packages/e6/68/6b71797d5f695fcc4f3c0e6337bfaf49840f914eaa7d06d517456ea07b96/connect-cli-24.0.tar.gz"
+  sha256 "c02b548ae247de10607a07a6d6508dbdfa4654813d798ec4e5acba2a8b9bfc25"
   license "Apache-2.0"
   revision 1
   head "https://github.com/cloudblue/connect-cli.git"
@@ -16,14 +16,14 @@ class ConnectCli < Formula
   depends_on "gdk-pixbuf"
   depends_on "libffi"
 
-  resource "pip" do
-    url "https://files.pythonhosted.org/packages/b7/2d/ad02de84a4c9fd3b1958dc9fb72764de1aa2605a9d7e943837be6ad82337/pip-21.0.1.tar.gz"
-    sha256 "99bbde183ec5ec037318e774b0d8ae0a64352fe53b2c7fd630be1d07e94f41e5"
-  end
-
   resource "ansicolors" do
     url "https://files.pythonhosted.org/packages/76/31/7faed52088732704523c259e24c26ce6f2f33fbeff2ff59274560c27628e/ansicolors-1.1.8.zip"
     sha256 "99f94f5e3348a0bcd43c82e5fc4414013ccc19d70bd939ad71e0133ce9c372e0"
+  end
+
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/dc/e7/7452227e8c091db6838d7f1b50dc4e6323e8463ed8c2d0b651ac9b7f7fce/anyio-3.4.0.tar.gz"
+    sha256 "24adc69309fb5779bc1e06158e143e0b6d2c56b302a3ac3de3083c705a6ed39d"
   end
 
   resource "appdirs" do
@@ -31,9 +31,24 @@ class ConnectCli < Formula
     sha256 "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
   end
 
+  resource "arrow" do
+    url "https://files.pythonhosted.org/packages/25/e2/85d4a709a3ea58f8e36b4db9eb7927560a2fa4b6f8f362fb6475962fec51/arrow-1.2.1.tar.gz"
+    sha256 "c2dde3c382d9f7e6922ce636bf0b318a7a853df40ecb383b29192e6c5cc82840"
+  end
+
+  resource "asgiref" do
+    url "https://files.pythonhosted.org/packages/07/93/3618b68b4ba6b54bc97b5fd7d90e4981471edfaf51c8321a29a3c76cf47c/asgiref-3.4.1.tar.gz"
+    sha256 "4ef1ab46b484e3c706329cedeff284a5d40824200638503f5768edb6de7d58e9"
+  end
+
   resource "attrs" do
     url "https://files.pythonhosted.org/packages/f0/cb/80a4a274df7da7b8baf083249b0890a0579374c3d74b5ac0ee9291f912dc/attrs-20.3.0.tar.gz"
     sha256 "832aa3cde19744e49938b91fea06d69ecb9e649c93ba974535d08ad92164f700"
+  end
+
+  resource "binaryornot" do
+    url "https://files.pythonhosted.org/packages/a7/fe/7ebfec74d49f97fc55cd38240c7a7d08134002b1e14be8c3897c0dd5e49b/binaryornot-0.4.4.tar.gz"
+    sha256 "359501dfc9d40632edc9fac890e19542db1a287bbcfa58175b66658392018061"
   end
 
   resource "cairocffi" do
@@ -72,13 +87,18 @@ class ConnectCli < Formula
   end
 
   resource "connect-openapi-client" do
-    url "https://files.pythonhosted.org/packages/65/17/4173f03b16e91049f682ab361140608ff296df4dc6435ddf8c2d0c3a3c11/connect-openapi-client-22.0.5.tar.gz"
-    sha256 "7d658f769ebc7390ea8d7b1b022d9bb4fde0298dd6cb957f6a4e4b4ea6bc029b"
+    url "https://files.pythonhosted.org/packages/65/f3/99d650ab7c77b91a7badc2f98bb5ed1daf3d039a44f118a407b8b73beb75/connect-openapi-client-24.0.tar.gz"
+    sha256 "c287eca6e8aebced383d3326e269bb2fd149cb7136f5f7b031797b9173085942"
   end
 
   resource "connect-reports-core" do
-    url "https://files.pythonhosted.org/packages/54/9e/6b3621213a6ae642c622100024a68ffd33f1a4efe71732402d93d51c10b7/connect-reports-core-23.0.3.tar.gz"
-    sha256 "2d94830837e947b20ca3039d6e98b0fa34999636ea1a93d0dc4e1ab1e7d11606"
+    url "https://files.pythonhosted.org/packages/4c/8f/2de7612976fae644fed0125def9413433c993d0a86b9ded0af5086be9735/connect-reports-core-23.0.12.tar.gz"
+    sha256 "a89ef716f9c428fff97f66dd3cbe0563c33bf0468aec5c1981f2745ea1d077bf"
+  end
+
+  resource "cookiecutter" do
+    url "https://files.pythonhosted.org/packages/58/f5/6f41fa38e6efe4a0e85771f99a4ad8c33b4c14f03b4cc53b459aac4a629a/cookiecutter-1.7.3.tar.gz"
+    sha256 "6b9a4d72882e243be077a7397d0f1f76fe66cf3df91f3115dbb5330e214fa457"
   end
 
   resource "cssselect2" do
@@ -86,24 +106,44 @@ class ConnectCli < Formula
     sha256 "93fbb9af860e95dd40bf18c3b2b6ed99189a07c0f29ba76f9c5be71344664ec8"
   end
 
+  resource "decorator" do
+    url "https://files.pythonhosted.org/packages/92/3c/34f8448b61809968052882b830f7d8d9a8e1c07048f70deb039ae599f73c/decorator-5.1.0.tar.gz"
+    sha256 "e59913af105b9860aa2c8d3272d9de5a56a4e608db9a2f167a8480b323d529a7"
+  end
+
   resource "defusedxml" do
     url "https://files.pythonhosted.org/packages/0f/d5/c66da9b79e5bdb124974bfe172b4daf3c984ebd9c2a06e2b8a4dc7331c72/defusedxml-0.7.1.tar.gz"
     sha256 "1bb3032db185915b62d7c6209c5a8792be6a32ab2fedacc84e01b52c51aa3e69"
   end
 
-  resource "et_xmlfile" do
+  resource "et-xmlfile" do
     url "https://files.pythonhosted.org/packages/22/28/a99c42aea746e18382ad9fb36f64c1c1f04216f41797f2f0fa567da11388/et_xmlfile-1.0.1.tar.gz"
     sha256 "614d9722d572f6246302c4491846d2c393c199cfa4edc9af593437691683335b"
   end
 
   resource "fs" do
-    url "https://files.pythonhosted.org/packages/74/98/abdd089384136e14166537d74185a61f764c0e5b7c87329d613cd3aa869f/fs-2.4.12.tar.gz"
-    sha256 "c10ba188b14d6213a1ca950efd004931abbfa64b294c80bbf1045753831bf42f"
+    url "https://files.pythonhosted.org/packages/15/e4/0b9d0647dd1953e5d934a9b889f745867afafdfbf4b8439f73b864e3d7e2/fs-2.4.13.tar.gz"
+    sha256 "caab4dc1561d63c92f36ee78976f6a4a01381830d8420ce34a78d4f1bb1dc95f"
+  end
+
+  resource "h11" do
+    url "https://files.pythonhosted.org/packages/bd/e9/72c3dc8f7dd7874812be6a6ec788ba1300bfe31570963a7e788c86280cb9/h11-0.12.0.tar.gz"
+    sha256 "47222cb6067e4a307d535814917cd98fd0a57b6788ce715755fa2b6c28b56042"
   end
 
   resource "html5lib" do
     url "https://files.pythonhosted.org/packages/ac/b6/b55c3f49042f1df3dcd422b7f224f939892ee94f22abcf503a9b7339eaf2/html5lib-1.1.tar.gz"
     sha256 "b2e5b40261e20f354d198eae92afc10d750afb487ed5e50f9c4eaf07c184146f"
+  end
+
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/6a/90/334411fe5455d30498add7d77a8bf4833bfc4671289a954fb2fd43795338/httpcore-0.13.7.tar.gz"
+    sha256 "036f960468759e633574d7c121afba48af6419615d36ab8ede979f1ad6276fa3"
+  end
+
+  resource "httpx" do
+    url "https://files.pythonhosted.org/packages/c6/57/4db75d83f350813414c2f52318862f52ce29bc0ebaa97b71c9202af32d79/httpx-0.18.2.tar.gz"
+    sha256 "9f99c15d33642d38bce8405df088c1c4cfd940284b4290cacbfb02e64f4877c6"
   end
 
   resource "idna" do
@@ -117,8 +157,8 @@ class ConnectCli < Formula
   end
 
   resource "interrogatio" do
-    url "https://github.com/cloudblue/homebrew-connect-cli/raw/main/resources/interrogatio-1.0.3.tar.gz"
-    sha256 "c49ef65b2a6f4f533cd0f5aea201e034d3d00699caf72102ce31fb9916387072"
+    url "https://files.pythonhosted.org/packages/04/66/7bd8c5a06ce9ee8ff73f2897a334673b590a34b00345768daf800d622337/interrogatio-2.1.0.tar.gz"
+    sha256 "721fe2d762d2562c0de65c11fdd91856b89756dcf927067a51056bef267aa781"
   end
 
   resource "iso3166" do
@@ -129,6 +169,11 @@ class ConnectCli < Formula
   resource "Jinja2" do
     url "https://files.pythonhosted.org/packages/4f/e7/65300e6b32e69768ded990494809106f87da1d436418d5f1367ed3966fd7/Jinja2-2.11.3.tar.gz"
     sha256 "a6d58433de0ae800347cab1fa3043cebbabe8baa9d29e668f1c768cb87a333c6"
+  end
+
+  resource "jinja2-time" do
+    url "https://files.pythonhosted.org/packages/de/7c/ee2f2014a2a0616ad3328e58e7dac879251babdb4cb796d770b5d32c469f/jinja2-time-0.2.0.tar.gz"
+    sha256 "d14eaa4d315e7688daa4969f616f226614350c48730bfa1692d2caebd8c90d40"
   end
 
   resource "jsonschema" do
@@ -156,6 +201,11 @@ class ConnectCli < Formula
     sha256 "6456a3b472e1ef0facb1129f3c6ef00713cebf62e736cd7a75bcc3247432f251"
   end
 
+  resource "orjson" do
+    url "https://files.pythonhosted.org/packages/f6/1e/61225fb33a9f614ac67f595a80008ded0bdc58ea20c0201703a166d37fca/orjson-3.6.4.tar.gz"
+    sha256 "f8dbc428fc6d7420f231a7133d8dff4c882e64acb585dcf2fda74bdcfe1a6d9d"
+  end
+
   resource "phonenumbers" do
     url "https://files.pythonhosted.org/packages/ac/8b/695f6490dede7a8021fac4b5c964f6c4e4e76a61aa7ed478caf4a85eacb7/phonenumbers-8.12.20.tar.gz"
     sha256 "ee5a8508c4a414262abad92ec33f050347f681973ed0fb36e98b52bfe159f6b8"
@@ -164,6 +214,11 @@ class ConnectCli < Formula
   resource "Pillow" do
     url "https://files.pythonhosted.org/packages/60/f0/dd2eb7911f948bf529f58f0c7931f6f6466f711bd6f1d81a69dc4edd4e2a/Pillow-8.1.2.tar.gz"
     sha256 "b07c660e014852d98a00a91adfbe25033898a9d90a8f39beb2437d22a203fc44"
+  end
+
+  resource "poyo" do
+    url "https://files.pythonhosted.org/packages/7d/56/01b496f36bbd496aed9351dd1b06cf57fd2f5028480a87adbcf7a4ff1f65/poyo-0.5.0.tar.gz"
+    sha256 "e26956aa780c45f011ca9886f044590e2d8fd8b61db7b1c1cf4e0869f48ed4dd"
   end
 
   resource "prompt-toolkit" do
@@ -181,7 +236,7 @@ class ConnectCli < Formula
     sha256 "2656e1a6edcdabf4275f9a3640db59fd5de107d88e8663c5d4e9a0fa62f77f94"
   end
 
-  resource "Pyphen" do
+  resource "pyphen" do
     url "https://files.pythonhosted.org/packages/61/8d/95a258a74cd44b2d5fce469e3fb2d774741778babbac35cf030ab32642b4/Pyphen-0.10.0.tar.gz"
     sha256 "719b21dfb4b04fbc11cc0f6112418535fe35474021120cccfffc43a25fe63128"
   end
@@ -191,9 +246,19 @@ class ConnectCli < Formula
     sha256 "2e636185d9eb976a18a8a8e96efce62f2905fea90041958d8cc2a189756ebf3e"
   end
 
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
+    sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
+  end
+
+  resource "python-slugify" do
+    url "https://files.pythonhosted.org/packages/bc/a4/57893fbaf7cbf303a4f2307564cf83855a5f2cc34544656e7263125a0d1e/python-slugify-5.0.2.tar.gz"
+    sha256 "f13383a0b9fcbe649a1892b9c8eb4f8eab1d6d84b84bb7a624317afa98159cab"
+  end
+
   resource "pytz" do
-    url "https://files.pythonhosted.org/packages/b0/61/eddc6eb2c682ea6fd97a7e1018a6294be80dba08fa28e7a3570148b4612d/pytz-2021.1.tar.gz"
-    sha256 "83a4a90894bf38e243cf052c8b58f381bfe9a7a483f6a9cab140bc7f702ac4da"
+    url "https://files.pythonhosted.org/packages/e3/8e/1cde9d002f48a940b9d9d38820aaf444b229450c0854bdf15305ce4a3d1a/pytz-2021.3.tar.gz"
+    sha256 "acad2d8b20a1af07d4e4c9d2e9285c5ed9104354062f275f3fcd88dcef4f1326"
   end
 
   resource "PyYAML" do
@@ -211,14 +276,34 @@ class ConnectCli < Formula
     sha256 "968089d4584ad4ad7c171454f0a5c6dac23971e9472521ea3b6d49d610aa6fc0"
   end
 
+  resource "rfc3986" do
+    url "https://files.pythonhosted.org/packages/79/30/5b1b6c28c105629cc12b33bdcbb0b11b5bb1880c6cfbd955f9e792921aa8/rfc3986-1.5.0.tar.gz"
+    sha256 "270aaf10d87d0d4e095063c65bf3ddbc6ee3d0b226328ce21e036f946e421835"
+  end
+
   resource "six" do
     url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
     sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
   end
 
+  resource "sniffio" do
+    url "https://files.pythonhosted.org/packages/a6/ae/44ed7978bcb1f6337a3e2bef19c941de750d73243fc9389140d62853b686/sniffio-1.2.0.tar.gz"
+    sha256 "c4666eecec1d3f50960c6bdf61ab7bc350648da6c126e3cf6898d8cd4ddcd3de"
+  end
+
+  resource "text-unidecode" do
+    url "https://files.pythonhosted.org/packages/ab/e2/e9a00f0ccb71718418230718b3d900e71a5d16e701a3dae079a21e9cd8f8/text-unidecode-1.3.tar.gz"
+    sha256 "bad6603bb14d279193107714b288be206cac565dfa49aa5b105294dd5c4aab93"
+  end
+
   resource "tinycss2" do
     url "https://files.pythonhosted.org/packages/ce/d3/ece7a98d5826bd134e269a3a3030153d30482194fca71d95a3041812aab8/tinycss2-1.1.0.tar.gz"
     sha256 "fbdcac3044d60eb85fdb2aa840ece43cf7dbe798e373e6ee0be545d4d134e18a"
+  end
+
+  resource "toml" do
+    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
+    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
   end
 
   resource "tqdm" do
@@ -231,12 +316,17 @@ class ConnectCli < Formula
     sha256 "e7b021f7241115872f92f43c6508082facffbd1c048e3c6e2bb9c2a157e28937"
   end
 
+  resource "validators" do
+    url "https://files.pythonhosted.org/packages/33/1a/4e4c12982b093796c1ceaff49cbc5998fb3a7866da755f8e7a1a40b8fda4/validators-0.18.2.tar.gz"
+    sha256 "37cd9a9213278538ad09b5b9f9134266e7c226ab1fede1d500e29e0a8fbb9ea6"
+  end
+
   resource "wcwidth" do
     url "https://files.pythonhosted.org/packages/89/38/459b727c381504f361832b9e5ace19966de1a235d73cdbdea91c771a1155/wcwidth-0.2.5.tar.gz"
     sha256 "c4d647b99872929fdb7bdcaa4fbe7f01413ed3d98077df798530e5b04f116c83"
   end
 
-  resource "WeasyPrint" do
+  resource "weasyprint" do
     url "https://files.pythonhosted.org/packages/db/7c/021c4011bd353650d1738bf1afd4793378a705d61962683160928b2e2e05/WeasyPrint-52.4.tar.gz"
     sha256 "8b648d35b1e828ce321078fe593ef9267c35280242fd8f673c91f48e417a4a5f"
   end
