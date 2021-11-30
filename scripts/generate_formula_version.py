0e94c9a586a712359c88c2b899f22a1812b8c67e
Francesco Faraone <francesco.faraone@cloudblue.com>
2021-11-30 16:01:38 +0100

    Fix formula version

--- scripts/generate_formula_version.py
+++ scripts/generate_formula_version.py
@@ -12,12 +12,16 @@ def generate():
     ]
 
     cli_info = graph['connect-cli']
-    if os.path.exists(
-        f'Formula/connect-cli@{cli_info["version"]}.rb',
-    ):
-        print(f'Formula for version {cli_info["version"]} already exists')
-        return
+    if os.path.exists('digest.txt'):
+        with open('digest.txt', 'r') as f:
+            digest = f.read()
+            if digest == cli_info['checksum']:
+                print(f'Formula for version {cli_info["version"]} already exists')
+                return
     
+    with open('digest.txt', 'w') as f:
+        f.write(cli_info['checksum'])
+
     env = Environment(
         loader=FileSystemLoader(searchpath='./scripts'),
         trim_blocks=True,
@@ -26,11 +30,11 @@ def generate():
     template = env.get_template('formula_template.rb.j2')
     formula = template.render(
         root_url=cli_info['url'],
-        root_version=cli_info['version'].replace('.', ''),
+        root_version=cli_info['version'],
         root_digest=cli_info['checksum'],
         resources=resources,
     )
-    with open(f'Formula/connect-cli@{cli_info["version"]}.rb', 'w') as f:
+    with open(f'Formula/connect-cli.rb', 'w') as f:
         f.write(formula)
     
     print(f'Formula for version {cli_info["version"]} has been generated!')
